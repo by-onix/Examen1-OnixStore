fea075ffee0f58c7d150aa339ba8a921aad003a9
by-onix <alfredo.aliaga@uab.edu.bo>
2025-08-04 22:02:03 -0400

    creado model de productos

--- backonix/routes/api.php
+++ backonix/routes/api.php
@@ -3,20 +3,21 @@
 //use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\MarcasController;
 use App\Http\Controllers\ProductoController;
 
 
 // Rutas públicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+// metodos marca producto:
+     Route::apiResource('marcas', MarcasController::class);
+     Route::apiResource('productos', ProductoController::class);
 
 // Rutas protegidas con Sanctum
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+//Route::middleware('auth:sanctum')->group(function () {
+  //  Route::get('/user', [AuthController::class, 'user']);
+    //Route::post('/logout', [AuthController::class, 'logout']);
 
-    // metodos marca producto:
-     Route::apiResource('marcas', MarcaController::class);
-     Route::apiResource('productos', ProductoController::class);
-});
+    
+//});

1794327becd960835aaa282d66373d1b3d70a2a0
by-onix <alfredo.aliaga@uab.edu.bo>
2025-08-04 20:52:50 -0400

    creado atributos de tabla marcas

--- backonix/database/migrations/2025_08_05_003509_create_marcas_table.php
+++ backonix/database/migrations/2025_08_05_003509_create_marcas_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('marcas', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre',50);
+            $table->string('descripcion',100);
             $table->timestamps();
         });
     }
